6a8611cbd97773742e4258bc2b2e7588fc9df036
Mohit Pundir <mpundir@ethz.ch>
2025-06-15 10:13:42 +0200

    finding a logical flow for the energy computation, using equinox

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="femsolver",
+    version="0.1.0",
+    author="Mohit Pundir",
+    author_email="mpundir@ethz.ch",
+    description="Functional programming and differentiable framework for finite element methods",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/yourusername/femsolver",  # optional
+    packages=find_packages(where="."),  # finds femsolver/
+    install_requires=[
+        "jax==0.4.30",
+        "equinox==0.12.1",
+        "numpy",
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.10",
+    include_package_data=True,
+)
