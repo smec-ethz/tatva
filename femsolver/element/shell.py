7fd03b3c0233afe1ed60827de8084e33ce21574a
florez <florez@ethz.ch>
2025-08-29 18:39:59 +0200

    wip(shell): end of week backup (not working)

--- femsolver/element/shell.py
+++ femsolver/element/shell.py
@@ -1,7 +1,9 @@
 from typing import NamedTuple
 
 import jax.numpy as jnp
-from jax import Array
+from jax import Array, vmap
+
+from femsolver.utils import auto_vmap
 
 from ._element import Element
 
@@ -28,6 +30,14 @@ class Shell4(Element):
         ]
     )
     quad_weights = jnp.array([1.0, 1.0, 1.0, 1.0])
+    shear_tying_points = jnp.array(
+        [
+            [0.0, -1.0],
+            [0.0, 1.0],
+            [-1.0, 0.0],
+            [1.0, 0.0],
+        ]
+    )
 
     def shape_function(self, xi: Array) -> Array:
         """Returns the shape functions evaluated at the local coordinates (xi, eta)."""
@@ -55,8 +65,9 @@ class Shell4(Element):
 
     def get_local_frame(self, J: Array) -> _LocalFrame:
         """Returns t1, t2, n, and the projection matrix P for the local frame defined by the Jacobian J."""
-        t1 = J[:, 0] / jnp.linalg.norm(J[:, 0])
-        t2_tmp = J[:, 2] - jnp.dot(J[:, 2], t1) * t1
+        g1, g2 = J[:, 0], J[:, 1]
+        t1 = g1 / jnp.linalg.norm(g1)
+        t2_tmp = g2 - jnp.dot(g2, t1) * t1
         t2 = t2_tmp / jnp.linalg.norm(t2_tmp)
         n = jnp.cross(t1, t2)
         P = jnp.vstack((t1, t2))
@@ -127,3 +138,182 @@ class Shell4(Element):
         dNdX = self.dNdX(xi, nodal_coords)
         # apply to nodal values: (3, nvalues)
         return dNdX @ nodal_values
+
+    def _get_geometrical_quantities(
+        self, xi: Array, nodal_coords: Array
+    ) -> tuple[Array, Array, _LocalFrame, Array]:
+        """Helper function to bundle operations. Returns shape functions, directional
+        derivatives, local frame, and Jacobian area at given local coords.
+        """
+        J, J_area = self.get_jacobian(xi, nodal_coords)  # (3, 2), ()
+        N = self.shape_function(xi)  # (4,)
+        directional_dN = self.directional_derivative(xi, nodal_coords)  # (2, 4)
+        frame = self.get_local_frame(J)
+        return N, directional_dN, frame, J_area
+
+    def _interpolate_kinematics(
+        self,
+        N: Array,
+        directional_dN: Array,
+        frame: _LocalFrame,
+        u: Array,
+        theta: Array,
+    ) -> tuple[Array, Array, Array, Array, Array]:
+        """Helper function to bundle operations. Returns displacements and rotations
+        at a given Gauss point."""
+        frame_basis = jnp.vstack((frame.t1, frame.t2, frame.n))  # (3, 3)
+        u_gp = (N[:, None] * u).sum(axis=0)
+        u_gp_local = frame_basis @ u_gp
+        dudX = u.T @ directional_dN.T  # (3, 4) @ (4, 2) -> (3, 2)
+
+        # directional derivatives of w (out-of-plane displacement) in tangential basis
+        dw = jnp.einsum("ij,i->j", dudX, frame.n)
+        # rotations about t1, t2 axes
+        theta_gp = (N[:, None] * theta).sum(axis=0)
+        # rotation derivatives in tangential basis
+        dtheta = theta.T @ directional_dN.T
+
+        return u_gp_local, dudX, dw, theta_gp, dtheta
+
+    def _mitc4_shear_at_tying_points(
+        self, nodal_coords: Array, u: Array, theta: Array
+    ) -> Array:
+        """Calculate the MITC4 shear strains at the shear tying points.
+
+        Args:
+            nodal_coords: Nodal coordinates of the element, shape (4, 3).
+            u: Nodal displacements of the element, shape (4, 3).
+            theta: Nodal rotations of the element, shape (4, 2).
+
+        Returns:
+            gamma_mitc: Shear strains at the shear tying points, shape (4, 2).
+        """
+
+        def gamma_at_gp(xi_tie: Array) -> Array:
+            N, directional_dN, frame, _ = self._get_geometrical_quantities(
+                xi_tie, nodal_coords
+            )
+            # TODO: optimize by spliting _interpolate_kinematics such that the unnecessary
+            # part is not computed
+            *_, dw, theta_gp, dtheta = self._interpolate_kinematics(
+                N, directional_dN, frame, u, theta
+            )
+            # shear strains in local frame
+            return dw - theta_gp
+
+        return vmap(gamma_at_gp)(self.shear_tying_points)
+
+    def energy_density_gp(
+        self, xi: Array, nodal_coords: Array, u: Array, theta: Array
+    ) -> tuple[Array, Array, Array]:
+        """Calculate the energy density at a given Gauss point.
+
+        Args:
+            xi: Local coordinates (r, s).
+            nodal_coords: Nodal coordinates of the element, shape (4, 3).
+            u: Nodal displacements of the element, shape (4, 3).
+            theta: Nodal rotations of the element, shape (4, 2).
+
+        Returns:
+            energy_density: Energy density at the given Gauss point, shape ().
+        """
+        N, directional_dN, frame, J_area = self._get_geometrical_quantities(
+            xi, nodal_coords
+        )
+        u_gp_local, dudX, dw, theta_gp, dtheta = self._interpolate_kinematics(
+            N, directional_dN, frame, u, theta
+        )
+        dudX_u = dudX[:2, :]  # in-plane displacement gradients
+        strain_membrane = 0.5 * (dudX_u + dudX_u.T + dudX_u.T @ dudX_u) + jnp.outer(
+            dw, dw
+        )
+        # bending curvatures
+        kappa = jnp.array(
+            [
+                dtheta[0, 0],
+                dtheta[1, 1],
+                dtheta[0, 1] + dtheta[1, 0],
+            ]
+        )
+        # shear strains (MITC4)
+        gamma = self.mitc4_shear(xi, nodal_coords, u, theta)
+
+        return strain_membrane, kappa, gamma
+
+    def get_membrane_strain(self, xi: Array, nodal_coords: Array, u: Array) -> Array:
+        """Calculate the membrane strain at the given local coordinates (xi, eta).
+
+        Args:
+            xi: Local coordinates (r, s).
+            nodal_coords: Nodal coordinates of the element, shape (4, 3).
+            u: Nodal displacements of the element, shape (4, 3).
+
+        Returns:
+            strain_membrane: Membrane strain at the given local coordinates, shape (2, 2).
+        """
+        dN_directional = self.directional_derivative(xi, nodal_coords)
+        dudX = u.T @ dN_directional.T
+
+        dw = jnp.einsum(
+            "ij,i->j",
+            dudX,
+            self.get_local_frame(self.get_jacobian(xi, nodal_coords)[0]).n,
+        )
+
+        dudX_u = dudX[:2, :]  # in-plane displacement gradients
+        strain_membrane = 0.5 * (dudX_u + dudX_u.T + dudX_u.T @ dudX_u) + jnp.outer(
+            dw, dw
+        )
+        return strain_membrane
+
+    def get_bending_curvature(
+        self, xi: Array, nodal_coords: Array, theta: Array
+    ) -> Array:
+        """Calculate the bending curvature at the given local coordinates (xi, eta).
+
+        Args:
+            xi: Local coordinates (r, s).
+            nodal_coords: Nodal coordinates of the element, shape (4, 3).
+            theta: Nodal rotations of the element, shape (4, 2).
+
+        Returns:
+            kappa: Bending curvature at the given local coordinates, shape (3,).
+        """
+        dN_directional = self.directional_derivative(xi, nodal_coords)
+        dtheta = theta.T @ dN_directional.T
+
+        kappa = jnp.array(
+            [
+                dtheta[0, 0],
+                dtheta[1, 1],
+                dtheta[0, 1] + dtheta[1, 0],
+            ]
+        )
+        return kappa
+
+    def mitc4_shear(
+        self, xi: Array, nodal_coords: Array, u: Array, theta: Array
+    ) -> Array:
+        """Calculate the MITC4 shear strains at the given local coordinates (xi, eta).
+
+        Args:
+            xi: Local coordinates (r, s).
+            nodal_coords: Nodal coordinates of the element, shape (4, 3).
+            u: Nodal displacements of the element, shape (4, 3).
+            theta: Nodal rotations of the element, shape (4, 2).
+
+        Returns:
+            gamma_mitc: Shear strains at the given local coordinates, shape (2,).
+        """
+        gamma_tying = self._mitc4_shear_at_tying_points(nodal_coords, u, theta)
+        r, s = xi
+        # TODO: check if this is correct
+        N_tie = jnp.array(
+            [
+                0.5 * (1.0 - s),
+                0.5 * (1.0 + s),
+                0.5 * (1.0 - r),
+                0.5 * (1.0 + r),
+            ]
+        )
+        return N_tie @ gamma_tying
