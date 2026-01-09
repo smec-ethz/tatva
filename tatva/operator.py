382714951796c920cf16be1ee5e9eac7da3a9e63
Mohit Pundir <tomohitpundir@gmail.com>
2026-01-09 12:45:48 +0100

    fix(operator): removes check on quad points dimnension to be equal to coords dimension, necessary for 2D elements in 3D space

--- tatva/operator.py
+++ tatva/operator.py
@@ -141,11 +141,6 @@ class Operator(Generic[ElementT], eqx.Module):
             )
 
         local_dim = quad_points.shape[1]
-        global_dim = coords.shape[1]
-        if local_dim > 1 and local_dim != global_dim:
-            raise ValueError(
-                f"Element {self.element.__class__.__name__} expects {local_dim}D coordinates but mesh provides {global_dim}D nodes."
-            )
         if local_dim == 0:
             raise ValueError("Element must have a positive number of local dimensions.")
 
